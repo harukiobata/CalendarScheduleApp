f83465d4b910ce14c5721e2c649c9622022c2e26
harukiobata <barukan0913@gmail.com>
2025-06-04 21:44:47 +0900

    add/バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, length: { in: 1..20 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
