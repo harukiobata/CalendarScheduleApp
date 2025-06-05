e55724e768c8ffc40990bb8ad15d4f4dd2d2493e
harukiobata <barukan0913@gmail.com>
2025-06-06 01:35:22 +0900

    add/activetimeをusers作成時にデフォルトで値が設定されるよう設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :active_times, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +7,19 @@ class User < ApplicationRecord
 
   validates :username, presence: true, length: { in: 1..20 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  after_create :create_default_active_times
+
+  private
+
+  def create_default_active_times
+    (1..5).each do |dow|
+      active_times.create!(
+        day_of_week: dow,
+        start_time: "09:00",
+        end_time: "17:00",
+        granularity_minutes: 30
+      )
+    end
+  end
 end
