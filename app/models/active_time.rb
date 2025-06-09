b48dde051f6af8a69586e8fd2d6b7a71be72194f
harukiobata <barukan0913@gmail.com>
2025-06-10 02:03:47 +0900

    add/開始と終了時間にnilブロックを追加し自作バリデーションにnilの時スルーするように調整

--- app/models/active_time.rb
+++ app/models/active_time.rb
@@ -1,12 +1,15 @@
 class ActiveTime < ApplicationRecord
   belongs_to :user
 
+  validates :start_time, :end_time, presence: true
   validates :granularity_minutes, inclusion: { in: [ 15, 30, 45, 60 ] }
   validate :start_time_before_end_time
 
   private
 
   def start_time_before_end_time
+    return if start_time.blank? || end_time.blank?
+
     if start_time > end_time
       errors.add(:start_time, "開始時間は終了時間より前にしてください")
     end
