5441695d1f222f55aeae4114ac95c576ebb2b618
harukiobata <barukan0913@gmail.com>
2025-06-10 00:01:17 +0900

    fix/使わないeditのルート削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "home/index"
   root "home#index"
   devise_for :users
-  resources :active_times, only: [ :index, :edit, :update ]
+  resources :active_times, only: [ :index, :update ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
