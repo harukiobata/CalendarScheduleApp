cf335b2e745ee3cd03357930d75f9396b4de75cc
harukiobata <barukan0913@gmail.com>
2025-06-10 00:02:50 +0900

    add/バリデーションの追加

--- app/models/active_time.rb
+++ app/models/active_time.rb
@@ -1,3 +1,14 @@
 class ActiveTime < ApplicationRecord
   belongs_to :user
+
+  validates :granularity_minutes, inclusion: { in: [15, 30, 45, 60] }
+  validate :start_time_before_end_time
+
+  private
+
+  def start_time_before_end_time
+    if start_time > end_time
+      errors.add(:start_time, "は終了時間より前にしてください")
+    end
+  end
 end
