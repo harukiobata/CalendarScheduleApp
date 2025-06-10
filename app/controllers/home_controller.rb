04fab8f95698a280f1c3af08aabb3242b66b6cc4
harukiobata <barukan0913@gmail.com>
2025-06-11 07:59:27 +0900

    add/eventを取得するよう設定

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @events = current_user ? current_user.events.order(:start_time) : Event.none
   end
 end
