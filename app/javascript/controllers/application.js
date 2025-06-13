04d3d3a47404bd7ada8029bbb65f06f441ad9374
harukiobata <barukan0913@gmail.com>
2025-06-14 02:04:14 +0900

    add/indexでのdropdownmenuのためにjsの追加

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,10 @@
+import "@hotwired/turbo-rails"
 import { Application } from "@hotwired/stimulus"
+import dropdownController from "./dropdown_controller"
 
 const application = Application.start()
+
+application.register("dropdown", dropdownController)
 
 // Configure Stimulus development experience
 application.debug = false
