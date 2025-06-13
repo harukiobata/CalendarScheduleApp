39e81fdefd6671ed6023ea80436d773b33dbf869
harukiobata <barukan0913@gmail.com>
2025-06-14 01:00:39 +0900

    add/requestspecで見ログインでエラーになるのでログインするように調整

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 RSpec.describe "Homes", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
+  before do
+    sign_in user
+  end
+
   describe "GET /index" do
     it "returns http success" do
       get "/home/index"
