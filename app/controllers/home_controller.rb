98d1836559c163d35f1be95b3b156ffcd3b194e2
harukiobata <barukan0913@gmail.com>
2025-06-20 14:36:34 +0900

    add&fix/スケジュールコントローラーの誤字を修正,home,scheduleで使うインスタンス変数をアプリケーションコントローラーに移行

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_schedule_data
 
   def index
+    @event = Event.new
     @events = current_user ? current_user.events.order(:start_time) : Event.none
   end
 end
---
+++ app/controllers/schedules_controller.rb
@@ -0,0 +1,21 @@
+class SchedulesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_schedule_data
+
+  def index
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "daily_schedule",
+          partial: "schedules/daily_schedule",
+          locals: {
+            base_date: @base_date,
+            dates: @dates,
+            grouped_events: @grouped_events,
+            date_active_times: @date_active_times
+          }
+        )
+      end
+    end
+  end
+end
