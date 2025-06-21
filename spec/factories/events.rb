4bf72e8276b8430465084e22293c210c55b46eca
harukiobata <barukan0913@gmail.com>
2025-06-21 13:03:35 +0900

    fix&change/factorybotのeventのカラムだったりの変更とactivetimeの修正

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,11 +1,12 @@
 FactoryBot.define do
   factory :event do
-    user { nil }
-    title { "MyString" }
-    description { "MyText" }
-    start_time { "2025-06-10 04:21:03" }
-    end_time { "2025-06-10 04:21:03" }
-    color { "MyString" }
-    location { "MyString" }
+    association :user
+    title { "予定タイトル" }
+    note { "メモ内容" }
+    location { "東京" }
+    category { "重要" }
+    date { "2025-7-10" }
+    start_time { "08:00" }
+    end_time { "09:30" }
   end
 end
