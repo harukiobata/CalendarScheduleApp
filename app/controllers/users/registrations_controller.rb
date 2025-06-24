d9b3923ee8a9e45c9681d141f9f560453bc57336
harukiobata <barukan0913@gmail.com>
2025-06-24 17:58:00 +0900

    add/ゲストログインのロジックとゲストログイン時の制限の追加

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :reject_guest_user, only: [:edit, :update, :destroy]
+
+  private
+
+  def reject_guest_user
+    if current_user.email == "guest@example.com"
+      redirect_to root_path, alert: "ゲストユーザーは編集,削除ができません"
+    end
+  end
+end
