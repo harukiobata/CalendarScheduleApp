6001b85f732c47bd35e38de2d16a0aed84eca055
harukiobata <barukan0913@gmail.com>
2025-07-08 22:43:37 +0900

    add/seedに必要な情報を追加しdatabaseの整理,readmeに追加記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,50 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.guest
+
+user = User.create!(
+  email: "user@example.com",
+  username: "一般ユーザー",
+  password: "password"
+)
+
+Event.create!([
+  {
+    title: "会議",
+    date: "2025-08-09",
+    start_time: "2025-08-09 10:00",
+    end_time: "2025-08-09 11:00",
+    category: "仕事",
+    user: user
+  },
+  {
+    title: "おでかけ",
+    date: "2025-08-11",
+    start_time: "2025-08-11 10:00",
+    end_time: "2025-08-11 16:00",
+    category: "プライベート",
+    user: user
+  },
+  {
+    title: "お祭り",
+    date: "2025-08-13",
+    start_time: "2025-08-13 18:00",
+    end_time: "2025-08-13 22:00",
+    category: "重要",
+    user: user
+  },
+  {
+    title: "散歩",
+    date: "2025-08-12",
+    start_time: "2025-08-12 9:00",
+    end_time: "2025-08-12 10:00",
+    category: "その他",
+    user: user
+  },
+  {
+    title: "会議"
+    date: "2025-07-25"
+    start_time: "2025-07-25 10:00"
+    end_time: "2025-07-25 12:00"
+    category: "仕事"
+    user: user
+  }
+])
