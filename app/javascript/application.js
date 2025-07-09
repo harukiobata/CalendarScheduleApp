f72223157991ed21984b62f071ffc256fa667b56
harukiobata <barukan0913@gmail.com>
2025-07-10 05:15:49 +0900

    fix&change/applicationを一つに統合

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import { Application } from "@hotwired/stimulus"
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+
+const application = Application.start()
+eagerLoadControllersFrom("controllers", application)
+
+application.debug = false
+window.Stimulus = application
+
+export { application }
