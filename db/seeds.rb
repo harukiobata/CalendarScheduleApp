2da6897118a5f80b07117aa6010b95d0d49e4c7c
harukiobata <barukan0913@gmail.com>
2025-07-14 17:26:33 +0900

    fix/rubocopで指摘された箇所の修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.guest
+guest_user = User.guest
 
 user = User.create!(
   email: "user@example.com",
@@ -8,34 +8,34 @@ user = User.create!(
 
 Event.create!([
   {
-    title: "会議",
+    title: "散歩",
     date: "2025-08-09",
-    start_time: "2025-08-09 10:00",
-    end_time: "2025-08-09 11:00",
-    category: "仕事",
-    user: user
-  },
-  {
-    title: "おでかけ",
-    date: "2025-08-11",
-    start_time: "2025-08-11 10:00",
-    end_time: "2025-08-11 16:00",
+    start_time: "2025-08-09 5:00",
+    end_time: "2025-08-09 7:00",
     category: "プライベート",
     user: user
   },
   {
-    title: "お祭り",
-    date: "2025-08-13",
-    start_time: "2025-08-13 18:00",
-    end_time: "2025-08-13 22:00",
+    title: "打ち合わせ",
+    date: "2025-08-11",
+    start_time: "2025-08-11 10:00",
+    end_time: "2025-08-11 16:00",
     category: "重要",
     user: user
   },
   {
-    title: "散歩",
+    title: "副業の仕事",
+    date: "2025-08-13",
+    start_time: "2025-08-13 19:00",
+    end_time: "2025-08-13 22:00",
+    category: "仕事",
+    user: user
+  },
+  {
+    title: "出張",
     date: "2025-08-12",
-    start_time: "2025-08-12 9:00",
-    end_time: "2025-08-12 10:00",
+    start_time: "2025-08-12 8:00",
+    end_time: "2025-08-12 22:00",
     category: "その他",
     user: user
   },
@@ -48,20 +48,20 @@ Event.create!([
     user: user
   },
   {
-    title: "打ち合わせ",
+    title: "副業の仕事",
     date: "2025-07-25",
     start_time: "2025-07-25 20:00",
     end_time: "2025-07-25 23:00",
     category: "仕事",
-    user: guest
+    user: guest_user
   },
   {
-    title: "勉強",
+    title: "クライアントとのすり合わせ",
     date: "2025-08-10",
     start_time: "2025-08-10 20:00",
-    end_time: "2025-08-10 23:30",
+    end_time: "2025-08-10 22:00",
     category: "重要",
-    user: guest
+    user: guest_user
   },
   {
     title: "散歩",
@@ -69,6 +69,6 @@ Event.create!([
     start_time: "2025-08-15 6:00",
     end_time: "2025-07-25 7:30",
     category: "プライベート",
-    user: guest
+    user: guest_user
   }
 ])
