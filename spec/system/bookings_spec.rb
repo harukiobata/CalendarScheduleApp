9e00f7eefeafbf0689b7494fbf935ed9d4aed6c6
harukiobata <barukan0913@gmail.com>
2025-08-21 01:46:37 +0900

    fix/rspecエラーの修正(waitを使用し,turboに対応)

--- spec/system/bookings_spec.rb
+++ spec/system/bookings_spec.rb
@@ -11,10 +11,12 @@ RSpec.describe "予約機能について", type: :system, js: true do
 
   it "リンクをクリックして入力フォームに移り名前とメールアドレスを入力して確定できる" do
     within(".schedule-week") do
+      expect(page).to have_css('a.time-slot', text: "12:00~12:30", wait: 5)
       first('a.time-slot', text: "12:00~12:30").click
     end
 
     within("#booking_frame") do
+      expect(page).to have_field("お名前", wait: 5)
       fill_in "お名前", with: "テストユーザー"
       fill_in "メールアドレス", with: "test@example.com"
       click_button "確定"
