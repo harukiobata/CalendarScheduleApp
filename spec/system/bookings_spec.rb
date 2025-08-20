9f8aab65b3373bfd71d8d57a8b9db428aa9538d8
harukiobata <barukan0913@gmail.com>
2025-08-21 01:57:52 +0900

    fix/rspecのエラーの修正

--- spec/system/bookings_spec.rb
+++ spec/system/bookings_spec.rb
@@ -6,6 +6,15 @@ RSpec.describe "予約機能について", type: :system, js: true do
 
   before do
     sign_in owner
+    visit active_times_path
+    click_link "・ 火曜日 00:00 - 23:59"
+    fill_in "開始時間", with: "08:00"
+    fill_in "終了時間", with: "20:00"
+    fill_in "予定予約機能の表示開始時間", with: "10:00"
+    fill_in "予定予約機能の表示終了時間", with: "15:30"
+    check "予定予約機能を有効にする"
+    check "この設定を全曜日に適用する"
+    click_button "設定を更新する"
     visit schedule_bookings_path(owner_id: owner)
   end
 
