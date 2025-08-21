d5f85140f6122d834ee7cc853f3b67d0ffac7e77
harukiobata <barukan0913@gmail.com>
2025-08-21 10:42:43 +0900

    fix/環境変数の齟齬の修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
     address:              "smtp.gmail.com",
     port:                 587,
     domain:               "gmail.com",
-    user_name:            ENV["SMTP_USERNAME"],  # Gmailアドレス
+    user_name:            ENV["SMTP_USER_NAME"],  # Gmailアドレス
     password:             ENV["SMTP_PASSWORD"],  # Gmailアプリパスワード
     authentication:       "plain",
     enable_starttls_auto: true
