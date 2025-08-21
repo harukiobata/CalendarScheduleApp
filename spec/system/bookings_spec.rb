db1a5ae65a97ddb77b8bbdeb4a0c3a11ad36053c
harukiobata <barukan0913@gmail.com>
2025-08-21 10:27:10 +0900

    fix/rspecの指摘点の修正

--- spec/system/bookings_spec.rb
+++ spec/system/bookings_spec.rb
@@ -5,16 +5,6 @@ RSpec.describe "予約機能について", type: :system, js: true do
   let!(:active_time) { create(:active_time, user: owner, timerex_enabled: true) }
 
   before do
-    sign_in owner
-    visit active_times_path
-    click_link "・ 火曜日 00:00 - 23:59"
-    fill_in "開始時間", with: "08:00"
-    fill_in "終了時間", with: "20:00"
-    fill_in "予定予約機能の表示開始時間", with: "10:00"
-    fill_in "予定予約機能の表示終了時間", with: "15:30"
-    check "予定予約機能を有効にする"
-    check "この設定を全曜日に適用する"
-    click_button "設定を更新する"
     visit schedule_bookings_path(owner_id: owner)
   end
 
